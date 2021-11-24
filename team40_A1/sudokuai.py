631672e68a8b7cd8e09b1060a3f8eb427a3f4b80
okivlo <i.vloothuis@gmail.com>
2021-11-24 14:25:19 +0100

    I cleaned up the functions and added proper comments so the input and output are clearer as well. possible now return the list of possible moves for any given board state. evaluate now returns an integer for a given boardstate

--- team40_A1/sudokuai.py
+++ team40_A1/sudokuai.py
@@ -21,56 +21,58 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
     def compute_best_move(self, game_state: GameState) -> None:
 
         # Get the standard needed variables
-        N = game_state.board.N          #depth of matrix
-        n = game_state.board.n          #number of rows in a block
-        m = game_state.board.m          #number of columns in a block
-
-        # make a 2d matrix representation
-        matrix = np.reshape(np.array(game_state.board.squares), (N,N))
+        N = game_state.board.N          # depth of matrix
+        n = game_state.board.n          # number of rows in a block
+        m = game_state.board.m          # number of columns in a block
 
         # Make a list of all the squares that have not yet been filled in
         open_squares = [(i,j) for i in range(N) for j in range(N) if game_state.board.get(i, j) == SudokuBoard.empty]
 
-        # checks which values are allowed for the given move. returns a list of all possible values for that coordinate
-        def possible(i, j):
-            # calculate sub-squares and prepare list of possible values
-            possible_values = list(range(1, N+1))        # This list wil eventually contain all the values possible on coordinate (i,j)
-            (p, q) = (np.int(np.ceil((i + 1) / n) * n)-1, np.int(np.ceil((j + 1) / m) * m)-1)   # calculates the lowest coordinates in the sub-square
-            (r, s) = (p-(n-1),q-(m-1))                                                          # calculates the highest coordinates in the sub-square
+        def convert_to_matrix(board: SudokuBoard):
+            """
+            @param board: A sudoku board.
+            @Return: a 2 array of the given board
+            """
+            matrix = np.reshape(np.array(board.squares), (N, N))
+            return matrix
 
-            # makes a list of all values in row/column and box
-            row_vals = np.unique(matrix[i, :])
-            col_vals = np.unique(matrix[:, j])
-            box_vals = np.unique(matrix[r:p, s:q])
-            all_values = np.concatenate((row_vals, col_vals, box_vals), axis=None)
+        def possible(board: SudokuBoard):
+            """
+            @param board: A sudoku board.
+            @Return: an array with all possible/legal moves in the Move format (x-coord, y-coord, value)
+            """
+            matrix = convert_to_matrix(board)
+            all_moves = []              # this will contain all the moves in the end
+            for coords in open_squares:  # loop over all empty squares
 
-            # remove all values in row/column/box from the list of possible values and return it
-            values_left = [x for x in possible_values if x not in np.unique(all_values)]
-            return values_left
+                # calculate sub-squares and prepare list of possible values
+                possible_values = list(range(1, N+1))        # This list wil eventually contain all the values possible on coordinate (i,j)
+                (p, q) = (np.int(np.ceil((coords[0] + 1) / n) * n)-1, np.int(np.ceil((coords[1] + 1) / m) * m)-1)   # calculates the highest coordinates in the sub-square
+                (r, s) = (p-(n-1),q-(m-1))                                                          # calculates the lowest coordinates in the sub-square
 
-        # This loop concatenates all open coordinate-squares with all possible values that it can fill in there
-        all_moves = []
-        for coords in open_squares:                         #loop over all empty squares
-            possibilities_list = possible(coords[0], coords[1])
+                # makes a list of all values in row/column and box
+                row_vals = np.unique(matrix[coords[0], :])
+                col_vals = np.unique(matrix[:, coords[1]])
+                box_vals = np.unique(matrix[r:p, s:q])
+                all_values = np.concatenate((row_vals, col_vals, box_vals), axis=None)
 
-            for x in range(len(possibilities_list)):
-                if TabooMove(coords[0], coords[1], possibilities_list[x]) in game_state.taboo_moves:
-                    continue
-                all_moves.append(Move(coords[0], coords[1], possibilities_list[x]))
+                # remove all values in row/column/box from the list of possible values and return it
+                values_left = [x for x in possible_values if x not in np.unique(all_values)]
+                for x in range(len(values_left)):
+                    if TabooMove(coords[0], coords[1], values_left[x]) in game_state.taboo_moves:
+                        continue
+                    all_moves.append(Move(coords[0], coords[1], values_left[x]))
 
-        print("These are all the possible moves: ")
-        for item in all_moves:
-            print(item)
-        print("amount of taboo_moves: ", len(game_state.taboo_moves))
+            return all_moves
 
-        move = random.choice(all_moves)
-        self.propose_move(move)
-
-        # input a board game-state
         def evaluate_board(board: SudokuBoard):
+            """
+            @param board: A sudoku board.
+            @Return: an integer with a numeric value. Higher = better board state
+            """
             final_score = 0     # This value will be the final score for the board evaluation, all subroutines add or subtract from this score
+            matrix = convert_to_matrix(board)
 
-            # calculate
             # These loops increase the evaluation score for each row/column that has one place left to fill in (it can increase our score)
             for i in range(N):
                 row_counter = 0
@@ -86,17 +88,24 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
                     final_score = final_score+1
 
             # calculate box_scores
-            # create all the subsquares
-            subsquares = [[matrix[j][i] for j in range(x, x + m) for i in range(y, y + n)] for x in range(0, N, m)for y in range(0, N, n)]
-            for i in range(len(subsquares)):
+            # create all the sub_squares
+            sub_squares = [[matrix[j][i] for j in range(x, x + m) for i in range(y, y + n)] for x in range(0, N, m)for y in range(0, N, n)]
 
-                if subsquares[i].count(0) == 1:
+            # Checks if there is only one zero in a sub-square and increases the counter if true
+            for i in range(len(sub_squares)):
+
+                if sub_squares[i].count(0) == 1:
                     final_score = final_score+1
 
             return final_score
 
-        evaluation_score = evaluate_board(game_state.board)     # This doesnt really matter as it uses the predefined matrix to calculate stuff
-        
+        evaluation_score = evaluate_board(game_state.board)
+
+        # This is how we eventually submit our proposed move
+        final_move_list = possible(game_state.board)
+        move = random.choice(final_move_list)
+        self.propose_move(move)
+
         while True:
             time.sleep(0.2)
-            self.propose_move(random.choice(all_moves))
+            self.propose_move(random.choice(final_move_list))

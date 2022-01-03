bf760eb386585a75a6fb474784f1ea3710815eea
20183046 <t.a.w.v.zoggel@student.tue.nl>
2022-01-03 12:54:17 +0100

    worked on Todo list point 1 and 3.
    1: implemented Iko's taboo move searcher
    maybe still want to implement Nikhil's legal move variant
    
    3: done
    
    Note: already quite heavy in computation time. TODO: todolist point 4, call possible() less times during computation.

--- team40_A3/sudokuai.py
+++ team40_A3/sudokuai.py
@@ -22,6 +22,17 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
 
     def compute_best_move(self, game_state: GameState):
 
+        # here we check whether points can be achieved with the next move
+        # if not and it looks like we are not going to end as the last player, change size by playing taboo move
+        legal_moves_dict, greediest_move_tuple = compute_greediest_move(game_state)
+        if len(get_all_empty_squares(game_state.board)) % 2 == 0 and greediest_move_tuple[3] == 0:
+            taboo_moves = get_taboo_moves(game_state.board)
+            while len(taboo_moves) != 0:
+                taboo_move = random.choice(taboo_moves)
+                if Move(taboo_move[0][0], taboo_move[0][1], taboo_move[1]) not in game_state.taboo_moves:
+                    self.propose_move(Move(taboo_move[0][0], taboo_move[0][1], taboo_move[1]))
+                    break
+
         # make a copy of the game_state so we do not ruin the original.
         game_state2 = copy.deepcopy(game_state)
         # Initialize root of the tree
@@ -32,6 +43,326 @@ class SudokuAI(competitive_sudoku.sudokuai.SudokuAI):
         self.propose_move(selected_node.parent_action)
         return
 
+
+def possible_allmoves(game_state: GameState):
+    # TODO: Add the functionality that also checks Nikhil's variant of legal moves
+    """
+    @Param game_state: A GameState item.
+    @Return: An array with all possible moves in the Move format (x-coord, y-coord, value).
+    """
+    all_moves = []  # this will contain all the moves in the end
+    open_squares = get_all_empty_squares(game_state.board)
+    for coords in open_squares:  # loop over all empty squares
+
+        values_left = list(range(1,
+                                 game_state.board.N + 1))  # This list wil eventually contain all the values possible on coordinate (i,j)
+        row_values, column_values = check_row_and_column_values(game_state.board,
+                                                                *coords)  # Get all values on row and column for given move
+        sub_square_values = check_sub_square_values(game_state.board,
+                                                    *coords)  # Get all values in sub-matrix for given move
+        joined_values = row_values + column_values + sub_square_values  # Put all values them together
+        remaining_moves = [x for x in values_left if
+                           x not in joined_values]  # Keep only the values that are not on the board yet
+        for value in remaining_moves:  # Add values to the list if not in tabooMove
+            if Move(coords[0], coords[1], value) not in game_state.taboo_moves:
+                all_moves.append(Move(coords[0], coords[1], value))
+    return all_moves
+
+
+def possible(game_state: GameState):
+    """
+   @Param game_state: A GameState item.
+   @Return: An array with all possible moves in the Move format (x-coord, y-coord, value).
+   """
+    taboo_moves = get_taboo_moves(game_state)
+    all_moves = possible_allmoves(game_state)
+
+    return_moves = []
+    for item in all_moves:
+        if item not in taboo_moves:
+            return_moves.append(item)
+    return return_moves
+
+
+def get_taboo_moves(game_state):
+    """
+    @Return: an array with some taboo_moves based on rows columns and submatrices,
+    format is [(x-coord, y-coord), value]
+    """
+    all_moves = possible_allmoves(game_state)  # Get all possible moves
+    single_value_coordinates = []  # Initialize the list of moves with only one value option
+    selected_items = [tuple([item.i, item.j]) for item in all_moves]  # Fill a list with only the coordinates, not the value
+    taboo_moves = []  # Initialize the return list
+
+    # Loop over all possible moves and extract the ones that only have one value option
+    for a in all_moves:
+        if selected_items.count(tuple([a.i, a.j])) == 1:
+            single_value_coordinates.append(a)
+
+    # Now check whether any other empty square on the same row, column or sub-square has the same value option,
+    # that is then a taboo_move
+    for single_moves in single_value_coordinates:
+        for move in all_moves:
+            if single_moves != move and single_moves.value == move.value and \
+                    (single_moves.j == move.j or single_moves.i == move.i or
+                     check_sub_square_values(game_state.board, single_moves.i, single_moves.j) ==
+                     check_sub_square_values(game_state.board, move.i, move.j)):
+                taboo_moves.append(move)
+    return taboo_moves
+
+def find_best_candidate_move(dict_of_moves: dict[int: tuple]) -> tuple:
+    """ Returns a tuple with coordinates of one of the best moves from the given move dictionary
+
+    :param dict_of_moves: dictionary with scores as index and move tuples as values
+    :return: tuple of one of the best scores in the given dictionary
+    """
+    # find the highest scoring move possible from the given dict
+    for index in range(3, -1, -1):
+        if dict_of_moves[f"score{index}"]:
+            return random.choice(dict_of_moves[f"score{index}"])
+
+
+def get_random_set_element(set_x: set) -> Any:
+    """ Given a set, return a random element from that set without changing the set
+    with the fastest Python implementation.
+
+    :param set_x: Given set to return a random element of
+    :return: Random element of set_x
+    """
+    elem = set_x.pop()
+    set_x.add(elem)
+
+    return elem
+
+def does_move_complete_row(
+        state: GameState,
+        i: int,
+        j: int,
+) -> Tuple[bool, set]:
+    """ Checks whether a move to the given field could complete a row
+    and also returns illegal numbers in that row
+
+    :param state: object of the current state of the game
+    :param i: row index of the move
+    :param j: column index of the move
+    :return: Tuple of the bool value representing
+     whether filling the cell gives the player a point for that row.
+    """
+    illegal_numbers_set = {0}
+    is_row_filled = True
+
+    # loop over the different columns of the row of the given cell,
+    # and check if there are any other empty cells.
+    for col_index in range(state.board.N):
+
+        # Skip the current cell (We already know it's empty)
+        if j == col_index:
+            continue
+
+        # Add the value of the cells
+        # in that row to the set of illegal numbers.
+        cell = state.board.get(i, col_index)
+        # Heuristic 1 - remove already played numbers in row from legal moves
+        illegal_numbers_set.add(cell)
+
+        # If a cell is empty, then filling another empty cell
+        # will not result in filling the row.
+        if cell == state.board.empty:
+            is_row_filled = False
+
+    return is_row_filled, illegal_numbers_set
+
+def does_move_complete_subgrid(
+        state: GameState,
+        i: int,
+        j: int,
+) -> Tuple[bool, set]:
+    """ Checks whether a move to the given field could complete
+     a subgrid and returns found illegal numbers in that subgrid
+
+    :param state: object of the current state of the game
+    :param i: row index of the move
+    :param j: column index of the move
+    :return: tuple of bool value representing whether filling
+    the cell gives the player a point for that subgrid and the set of
+    illegal (taken) numbers from that subgrid
+    """
+    # boolean that represents if filling the cell
+    # gives the player a point for that subgrid.
+    is_subgrid_filled = True
+    illegal_numbers_set = {0}
+
+    # Compute the starting and ending row
+    # and column indices for the subgrid of the cell.
+    row_subgrid_start = (i // state.board.m) * state.board.m
+    row_subgrid_end = ((i // state.board.m) + 1) * state.board.m
+    col_subgrid_start = (j // state.board.n) * state.board.n
+    col_subgrid_end = ((j // state.board.n) + 1) * state.board.n
+
+    # loop over the cells of the subgrid
+    # and check if there are any other empty cells.
+    for subgrid_row_index in range(row_subgrid_start, row_subgrid_end):
+
+        for subgrid_col_index in range(col_subgrid_start,
+                                       col_subgrid_end):
+
+            # Skip the current cell (We already know it's empty)
+            if i == subgrid_row_index and j == subgrid_col_index:
+                continue
+
+            # Add the numbers in the cell's subgrid
+            # to the illegal_numbers set
+            cell = state.board.get(
+                subgrid_row_index, subgrid_col_index)
+            # Heuristic 1 - remove already played numbers in subgrid from legal moves
+            illegal_numbers_set.add(cell)
+
+            # If a cell in the subgrid is empty,
+            # then filling the given cell
+            # will result in no points for that subgrid
+            if cell == state.board.empty:
+                is_subgrid_filled = False
+
+    return is_subgrid_filled, illegal_numbers_set
+
+def does_move_complete_column(
+        state: GameState,
+        i: int,
+        j: int,
+) -> Tuple[bool, set]:
+    """ Checks whether a move to the given field could complete a column
+    and updates the illegal numbers set on the column
+
+    :param state: object of the current state of the game
+    :param i: row index of the move
+    :param j: column index of the move
+    :return: tuple of bool value representing whether filling the cell
+     gives the player a point for that column and the set of illegal
+     numbers from that column
+    """
+    illegal_numbers_set = {0}
+    is_col_filled = True
+    # loop over the different rows of the column of the given cell,
+    # and check if there are any other empty cells.
+    for row_index in range(state.board.N):
+
+        # Skip the current cell (We already know it's empty)
+        if i == row_index:
+            continue
+
+        # Add the value of the cells
+        # in that column to the set of illegal numbers.
+        cell = state.board.get(row_index, j)
+        # Heuristic 1 - remove already played numbers in column from legal moves
+        illegal_numbers_set.add(cell)
+
+        # If a cell is empty, then filling another empty cell
+        # will not result in filling the column.
+        if cell == state.board.empty:
+            is_col_filled = False
+
+    return is_col_filled, illegal_numbers_set
+
+def get_legal_moves(state: GameState, return_type: str) \
+        -> Union[dict, list]:
+    """ Returns list or dictionary of legal moves,
+    depending on the requested return_type
+
+    :param state: state of the game for which the moves are requested
+    :param return_type: 'dict' or 'list' corresponding
+     to the result type needed
+    :return: Either a dictionary with scores as keys and corresponding
+     moves as values if dict is requested,
+     or simple list of legal moves otherwise
+    """
+    N = state.board.N
+
+    # Initializing the dictionary that will contain the best moves.
+    # Each of the scores shown below will contain
+    # a list of tuples of the form (i ,j , value, score)
+    # The 'i' and 'j' are the row and column indices of the cell.
+    # Value is the number the cell is to be filled with.
+    # Score refers to how good a move is right now -in a greedy manner.
+    # score 0 means that playing the given move will result in 0 points
+    # score 1 means that playing the given move will result in 1 point
+    # score 2 means that playing the given move will result in 2 points
+    # score 3 means that playing the given move will result in 7 points
+
+    best_move_dict = {
+        'score0': [],
+        'score1': [],
+        'score2': [],
+        'score3': [],
+    }
+
+    # The set of all possible numbers given an empty board.
+    possible_numbers = {
+        number for number in range(state.board.N + 1)}
+
+    # Loop over all cells in the board, find empty cells and
+    # compute cell_info for each one.
+    for i in range(N):
+        for j in range(N):
+            cell = state.board.get(i, j)
+            if cell == state.board.empty:
+
+                # Set of numbers that we CAN'T use for this empty cell.
+                illegal_numbers = {0}
+                elem = None
+
+                # check whether filling this cell will
+                # complete a column, row, or subgrid
+                # note: We pass the illegal_numbers set by reference
+                # so we can compute the numbers already played
+                # in a column, row and subgrid,
+                # which saves an extra iteration later
+                will_fill_column, illegal_numbers_col = \
+                    does_move_complete_column(state, i, j)
+                will_fill_row, illegal_numbers_row = \
+                    does_move_complete_row(state, i, j)
+                will_fill_subgrid, illegal_numbers_subgrid = \
+                    does_move_complete_subgrid(state, i, j)
+
+                illegal_numbers = illegal_numbers.union(
+                    illegal_numbers_col,
+                    illegal_numbers_row,
+                    illegal_numbers_subgrid
+                )
+
+                # Heuristic 2 - assign score to every legal move
+                score = will_fill_column + will_fill_row + will_fill_subgrid
+
+                # The set of legal numbers for a given cell, is
+                # the set of all possible moves - set of illegal moves
+                legal_numbers = possible_numbers.difference(illegal_numbers)
+
+                try:
+                    elem = get_random_set_element(legal_numbers)
+                except KeyError:
+                    continue
+
+                # add the move to the best move list
+                # (if it's not a TabooMove)
+                if not (TabooMove(i, j, elem) in state.taboo_moves):
+                    best_move_dict[f"score{score}"].append(
+                        (i, j, elem, score)
+                    )
+
+    if return_type == "dict":
+        return best_move_dict
+    elif return_type == "list":
+        return get_legal_move_list_from_dict(best_move_dict)
+
+def compute_greediest_move(state: GameState) -> tuple:
+    """ Computes best move according to the greedy player's strategy
+
+    :param state: current state of the game
+    :return: tuple of the legal moves (with scores as keys)
+     and the best move according to the greedy strategy
+    """
+    best_move_dict = get_legal_moves(state, "dict")
+
+    return best_move_dict, find_best_candidate_move(best_move_dict)
 
 def check_row_and_column_values(board: SudokuBoard, x, y):
     """
@@ -73,33 +404,6 @@ def check_sub_square_values(board: SudokuBoard, x, y):
         for j in range(s, q + 1):
             sub_matrix_values.append(board.get(i, j))
     return sub_matrix_values
-
-
-def possible(game_state: GameState):
-    # TODO: Add the functionality that makes it so we only consider valid/legal moves. Not all possible moves.
-    """
-    @Param game_state: A GameState item.
-    @Return: An array with all possible moves in the Move format (x-coord, y-coord, value).
-    """
-    all_moves = []  # this will contain all the moves in the end
-    open_squares = get_all_empty_squares(game_state.board)
-    for coords in open_squares:  # loop over all empty squares
-
-        values_left = list(range(1,
-                                 game_state.board.N + 1))  # This list wil eventually contain all the values possible on coordinate (i,j)
-        row_values, column_values = check_row_and_column_values(game_state.board,
-                                                                *coords)  # Get all values on row and column for given move
-        sub_square_values = check_sub_square_values(game_state.board,
-                                                    *coords)  # Get all values in sub-matrix for given move
-        joined_values = row_values + column_values + sub_square_values  # Put all values them together
-        remaining_moves = [x for x in values_left if
-                           x not in joined_values]  # Keep only the values that are not on the board yet
-        for value in remaining_moves:  # Add values to the list if not in tabooMove
-            if Move(coords[0], coords[1], value) not in game_state.taboo_moves:
-                all_moves.append(Move(coords[0], coords[1], value))
-
-    return all_moves
-
 
 def score_eval(board: SudokuBoard, move: Move):
     """
@@ -302,7 +606,7 @@ class MonteCarloTreeSearchNode:
         After that it performs the rollout, followed by the backpropagation.
         Change simulation_no to change the random sample size.
         """
-        simulation_no = 1000
+        simulation_no = 100
         for i in range(simulation_no):
             v = self._tree_policy()
             reward = v.rollout()
